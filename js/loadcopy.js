f22def51daf41f19dd189180dfff90d11e58745e
envenuslu <pisscoisdum@gmail.com>
2026-01-04 15:58:01 -0800

    k

---
+++ js/loadcopy.js
@@ -0,0 +1,16 @@
+function loadComponent(elementId, filePath) {
+    fetch(filePath)
+        .then(response => {
+            if (!response.ok) throw new Error('Failed to load ' + filePath);
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error(error));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadComponent('navbarplaceholder', '/copy/navbar.html');
+    loadComponent('footerplaceholder', '/copy/footer.html');
+});
